e4b8fef01cfbff06f9743ba9cceb351c01da4f81
madalin560 <alexandrustanel@yahoo.com>
2020-05-03 20:21:32 +0300

    Add base search functionability

--- ui/src/components/app-menu/AppMenu.jsx
+++ ui/src/components/app-menu/AppMenu.jsx
@@ -7,9 +7,31 @@ import {
     FormControl,
     Button
 } from 'react-bootstrap';
+import _ from 'lodash';
+
+import './AppMenu.scss';
 
 class AppMenu extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            moviesResult: []
+        }
+    }
+
+    handleMovieSearch = (event) => {
+        const formInput = event.currentTarget.elements[0];
+        const searchQuery = _.get(formInput, 'value', null);
+
+        fetch(`http://localhost:8080/searchMovie?title=${searchQuery}`).then(response => response.json())
+            .then(data => this.setState({
+                moviesResult: _.get(data, 'Search', [])
+            }));
+    };
+
     render() {
+        
         return (
             <Navbar bg="light" expand="lg">
                 <Navbar.Brand>AMD</Navbar.Brand>
@@ -25,9 +47,20 @@ class AppMenu extends React.Component {
                         <NavDropdown.Item>Separated link</NavDropdown.Item>
                     </NavDropdown>
                     </Nav>
-                    <Form inline>
+                    <Form inline onSubmit={this.handleMovieSearch}>
                         <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                        <Button variant="outline-success">Search</Button>
+                        {!_.isEmpty(this.state.moviesResult) && (
+                            <div className="film-results">
+                                {
+                                    this.state.moviesResult.map(movie => (
+                                        <div className="movie">
+                                            {_.get(movie, 'Title', '-')}    
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        )}
+                        <Button type="submit" variant="outline-success">Search</Button>
                     </Form>
                 </Navbar.Collapse>
             </Navbar>
