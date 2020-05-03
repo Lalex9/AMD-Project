916e2cd875043ddf3f2ed06d84063c77695d096b
Lungu.Alexandru <l.alex1999@yahoo.com>
2020-05-03 17:48:51 +0300

    Add Cross header

--- src/main/java/com/example/demo/AmdApplication.java
+++ src/main/java/com/example/demo/AmdApplication.java
@@ -3,10 +3,7 @@ package com.example.demo;
 import com.api.OMDbWebServiceClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
