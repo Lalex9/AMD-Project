f19f82595d5dc7119209f5975bd3962be616655a
diana379 <38556155+diana379@users.noreply.github.com>
2020-05-07 23:35:22 +0300

    Add content for home page
    * Update movie descriptions Homepage
    
    * Add content for home page

--- ui/src/pages/Homepage.jsx
+++ ui/src/pages/Homepage.jsx
@@ -1,53 +1,47 @@
 import React from 'react';
 import {
-    Button,
     Container,
     CardDeck,
     Card,
     Jumbotron
 } from 'react-bootstrap';
 
+
 class Homepage extends React.Component {
     render() {
         return (
             <React.Fragment>
                 <Jumbotron>
-                    <h1>Hello, world!</h1>
-                    <p>
-                        This is a simple hero unit, a simple jumbotron-style component for calling
-                        extra attention to featured content or information.
-                    </p>
-                    <p>
-                        <Button variant="primary">Learn more</Button>
-                    </p>
+                    <img id = "picture" src={require('../static/bond.jpg')} alt = "James Bond" />
+
                 </Jumbotron>
                 <Container fluid>
                     <h3>Trending Movies</h3>
                     <CardDeck>
                         <Card>
-                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Img className = "pics" variant="top" src={require('../static/joker.jpg')} />
                             <Card.Body>
-                                <Card.Title>Movie 1</Card.Title>
+                                <Card.Title>Joker</Card.Title>
                                 <Card.Text>
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                In Gotham City, mentally troubled comedian Arthur Fleck is disregarded and mistreated by society. He then embarks on a downward spiral of revolution and bloody crime. This path brings him face-to-face with his alter-ego: the Joker.
                                 </Card.Text>
                             </Card.Body>
                         </Card>
                         <Card>
-                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Img className = "pics" variant="top" src={require('../static/parasite.jpg')} />
                             <Card.Body>
-                                <Card.Title>Movie 2</Card.Title>
+                                <Card.Title>Parasite</Card.Title>
                                 <Card.Text>
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan.
                                 </Card.Text>
                             </Card.Body>
                         </Card>
                         <Card>
-                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Img className = "pics" variant="top" src={require('../static/knivesout.jpg')} />
                             <Card.Body>
-                                <Card.Title>Movie 3</Card.Title>
+                            <Card.Title>Knives out</Card.Title>
                                 <Card.Text>
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                A detective investigates the death of a patriarch of an eccentric, combative family.
                                 </Card.Text>
                             </Card.Body>
                         </Card>
